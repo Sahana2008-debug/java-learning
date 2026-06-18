3d7e5dbbfcd758d425de831c68ebc7cf9334d50e
sahana2008-debug <sahana90259@gmail.com>
2026-06-18 19:32:00 +0530

    java program

--- for loop/q2.java
+++ for loop/q2.java
@@ -1,1 +1,16 @@
-//Get input 
+//Get input for a variable a and b and print the number from a to b
+import java.util.Scanner;
+class q2
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a and b :");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        for (int i=a;i<=b;i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
