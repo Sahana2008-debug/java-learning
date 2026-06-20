2426cc5f9a4fa6c85cf91b3ab5360d53934e0164
sahana2008-debug <sahana90259@gmail.com>
2026-06-20 11:37:54 +0530

    java program

---
+++ for loop/q7.java
@@ -0,0 +1,18 @@
+/* print this output
+*
+**
+*** */
+class q7
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=3;i++)
+        {
+            for(int j =1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
